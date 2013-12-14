6f66806aeb5f36007739067d5ed4c3346d15ecd8
alessioperry <ale.pam@gmail.com>
2013-12-14 23:28:49 +0100

    First Commit
    First commit after code retrat

---
+++ GameOfLifeStage3.Business.Test/JesusChristTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GameOfLifeStage3.Business.Test
+{
+    [TestFixture]
+    public class JesusChristTest
+    {
+        private Cell second;
+        private Cell first;
+        private Jesus jesus;
+
+        [SetUp]
+        public void SetUp()
+        {
+            first = new Cell();
+            second = new Cell();
+
+            jesus = new Jesus(first);
+        }
+
+        [Test]
+        public void IfYouHaveOnlyOneCellAliveItDies()
+        {
+            jesus.Evolve();
+
+            Assert.False(first.IsAlive());
+        }
+
+        [Test]
+        public void IfYouHaveTwoCellsTheyDiesTogheter()
+        {
+            first.AddNeighbour(second);
+
+            jesus.Evolve();
+
+            Assert.False(first.IsAlive());
+        }
+
+    }
+
+    internal enum Status
+    {
+        Alive,Dead
+    }
+
+    public class Cell
+    {
+        private Status status;
+        private List<Cell> neighbours;
+
+        public Cell()
+        {
+            status = Status.Alive;
+        }
+
+        public void AddNeighbour(Cell cell)
+        {
+            if (neighbours != null)
+            neighbours.Add(cell);
+
+            neighbours = new List<Cell> { cell };
+        }
+
+        public int NeighbourCount()
+        {
+            return neighbours != null ? neighbours.Count : 0;
+        }
+
+        public void Killme()
+        {
+            status = Status.Dead;
+        }
+
+        public bool IsAlive()
+        {
+            return status == Status.Alive;
+        }
+    }
+
+    internal class Jesus
+    {
+        private readonly Cell cell;
+
+        public Jesus(Cell cell)
+        {
+            this.cell = cell;
+        }
+
+        public void Evolve()
+        {
+           if(cell.NeighbourCount() < 2)
+            cell.Killme();
+        }
+    }
+}
